efe66694cde695a67a9c2eceaa87c695e4368ef0
Tim <tim.tannert@tu-dortmund.de>
2019-10-02 18:10:22 +0200

    Effiziente Prüfung auf bereits vorhandene Werte

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,33 @@ use uint::Typable;
 const SEED: u128 = 0xcafef00dd15ea5e5;
 
 fn main() {
+
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        println!("Bitte genau ein Argument übergeben!");
+    }
+
     let gen_start = Instant::now();
-    println!("Starte generierung der zufälligen Werte");
-    generate_uniform_distribution::<u40>(32);
-    println!("Gleichverteilung erzeugt in {} Sekunden",gen_start.elapsed().as_secs());
-    let gen_normal = Instant::now();
-    generate_normal_distribution::<u40>(32, (1u64<<39) as f64, (1u64<<37) as f64, "bereich_komplett");
-    println!("Normalverteilung erzeugt in {} Sekunden",gen_normal.elapsed().as_secs());
 
-    let gen_normal = Instant::now();
-    generate_normal_distribution::<u40>(32, (1u64<<39) as f64, (1u64<<35) as f64, "bereich_viertel");
-    println!("Normalverteilung erzeugt in {} Sekunden",gen_normal.elapsed().as_secs());
-
-    
-
-    println!("Gesamtlaufzeit: {} Sekunden",gen_start.elapsed().as_secs());
+    match args[1].as_ref() {
+        "normal_komplett" => {
+            generate_normal_distribution::<u40>(32, (1u64<<39) as f64, (1u64<<37) as f64, "bereich_komplett");
+            println!("Normalverteilung erzeugt in {} Sekunden",gen_start.elapsed().as_secs());
+        },
+        "normal_viertel" => {
+            generate_normal_distribution::<u40>(32, (1u64<<39) as f64, (1u64<<35) as f64, "bereich_viertel");
+            println!("Normalverteilung erzeugt in {} Sekunden",gen_start.elapsed().as_secs());
+        },
+        "uniform" => {
+            println!("Starte generierung der zufälligen Werte");
+            generate_uniform_distribution::<u40>(32);
+            println!("Gleichverteilung erzeugt in {} Sekunden",gen_start.elapsed().as_secs());
+        }
+        _ => {
+            println!("Bitte verwende {} <normal_komplett|normal_viertel|uniform>",args[0]);
+        }
+    };
 }
 
 /// Diese Methode generiert 2^`exponent`viele unterschiedliche sortierte Zahlen vom Typ u40, u48 und u64.AsMut
@@ -62,6 +74,9 @@ fn generate_uniform_distribution<T: Typable + Serialize + Ord + Copy + Into<u64>
 /// Diese Methode generiert 2^`exponent`viele normalverteilte sortierte Zahlen vom Typ u40, u48 und u64.AsMut
 /// Dabei werden Dateien von 2^0 bis hin zu 2^`exponent` angelegt.
 fn generate_normal_distribution<T: Typable + Serialize + Ord + Copy + Into<u64> + From<u64>>(exponent: u64, mean: f64, deviation: f64, name: &str) {
+
+    // Dieses Bitarray hat für jeden möglichen u40 einen Bit als Eintrag, der angibt, ob dieser Wert bereits gesammelt wurde
+    let mut memory = vec![0u64;(T::max_value().into()/64) as usize];
     // Erzeugt die testdata Directorys, falls diese noch nicht existieren.
     create_dir_all(format!("./testdata/normal/{}/{}/",name,T::TYPE)).unwrap();
 
@@ -71,11 +86,12 @@ fn generate_normal_distribution<T: Typable + Serialize + Ord + Copy + Into<u64> 
     let mut result: Vec<T> = Vec::with_capacity(max_value); 
     for _ in 0..max_value {
         let mut random_val = normal.sample(&mut rng);
-        while random_val < 0.0 || (random_val as u64) > T::max_value().into() {
+        while random_val < 0.0 || (random_val as u64) > T::max_value().into() || contains(random_val as u64, &memory) {
             random_val = normal.sample(&mut rng);
         }
 
         result.push((random_val as u64).into());
+        set_value(random_val as u64, &mut memory);
         
     }
 
@@ -98,4 +114,20 @@ fn generate_normal_distribution<T: Typable + Serialize + Ord + Copy + Into<u64> 
 fn write_to_file<T: Typable + Serialize>(name: String, val: &Vec<T>) {
     let mut buf = BufWriter::new(File::create(name).unwrap());
     val.serialize(&mut Serializer::new(&mut buf)).unwrap();
+}
+
+#[inline]
+fn contains(val: u64, memory: &Vec<u64>) -> bool {
+    let index = (val/64) as usize;
+    let in_index = val%64;
+    let mask = 1u64<<(63-in_index);
+    (memory[index] & mask) != 0
+}
+
+#[inline]
+fn set_value(val: u64, memory: &mut Vec<u64>) {
+    let index = (val/64) as usize;
+    let in_index = val%64;
+    let mask = 1u64<<(63-in_index);
+    memory[index] = memory[index] | mask;
 }
